13a2d5985f652734c800edc81590f5f581d0124c
Lucas Paulo <lucaspaulo1@hotmail.com>
2025-06-22 22:42:12 -0300

    implementacao dos testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -46,7 +46,7 @@ describe('TODOMvc App', () => {
       .first()
       .click();
 
-    cy.get('[data-cy=filter-active-link')
+    cy.get('[data-cy=filter-active-link]')
       .click();
     cy.get('[data-cy=todos-list]')
       .children()
@@ -54,7 +54,7 @@ describe('TODOMvc App', () => {
       .first()
       .should('have.text', 'Prova de ES');
 
-    cy.get('[data-cy=filter-completed-link')
+    cy.get('[data-cy=filter-completed-link]')
       .click();
     cy.get('[data-cy=todos-list]')
       .children()
@@ -62,10 +62,77 @@ describe('TODOMvc App', () => {
       .first()
       .should('have.text', 'TP2 de ES');
 
-    cy.get('[data-cy=filter-all-link')
+    cy.get('[data-cy=filter-all-link]')
       .click();
     cy.get('[data-cy=todos-list]')
       .children()
       .should('have.length', 2);
+  });
+
+  it('Marca uma tarefa como completa e verifica estilo', () => {
+    cy.visit('');
+    
+    cy.get('[data-cy=todo-input]')
+      .type('Tarefa para completar{enter}');
+    
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .click();
+    
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .should('have.class', 'completed');
+  });
+
+  it('Edita uma tarefa existente', () => {
+    cy.visit('');
+    
+    cy.get('[data-cy=todo-input]')
+      .type('Tarefa editável{enter}');
+    
+    cy.get('[data-cy=todos-list] > li label')
+      .first()
+      .dblclick();
+    
+    cy.get('[data-cy=todos-list] > li .edit')
+      .first()
+      .clear()
+      .type('Tarefa modificada{enter}');
+    
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .should('contain', 'Tarefa modificada');
+  });
+
+  it('Limpa todas as tarefas completas', () => {
+    cy.visit('');
+    
+    // 1. Criar 3 tarefas (usando o mesmo padrão dos outros testes)
+    cy.get('[data-cy=todo-input]')
+      .type('Tarefa 1{enter}')
+      .type('Tarefa 2{enter}')
+      .type('Tarefa 3{enter}');
+    
+    // 2. Marcar as duas primeiras como completas (padronizado com outros testes)
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .find('[data-cy=toggle-todo-checkbox]')
+      .click();
+    
+    cy.get('[data-cy=todos-list] > li')
+      .eq(1)
+      .find('[data-cy=toggle-todo-checkbox]')
+      .click();
+    
+    // 3. Clicar no botão de limpar (Atenção: usando a classe real .clear-completed)
+    cy.get('.clear-completed')
+      .should('be.visible')
+      .click();
+    
+    // 4. Verificar resultado (mesmo padrão dos outros asserts)
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Tarefa 3');
   });
 });
